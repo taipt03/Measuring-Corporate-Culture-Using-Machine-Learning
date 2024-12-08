9911079e06475330f00f5660684aab9b767bf74d
Tai Pham <39149250+taipt03@users.noreply.github.com>
2024-12-08 21:07:40 +0700

    modify

--- clean_and_train.py
+++ clean_and_train.py
@@ -82,7 +82,7 @@ culture_models.train_w2v_model(
         global_options.DATA_FOLDER, "processed", "trigram", "documents.txt"
     ),
     model_path=Path(global_options.MODEL_FOLDER, "w2v", "w2v.mod"),
-    size=global_options.W2V_DIM,
+    vector_size=global_options.W2V_DIM,
     window=global_options.W2V_WINDOW,
     workers=global_options.N_CORES,
     iter=global_options.W2V_ITER,
