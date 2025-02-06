844f77662baaaafd40f67b5cd32027e012626d24
Tai Pham <39149250+taipt03@users.noreply.github.com>
2025-02-06 14:19:47 +0700

    Update parse_parallel.py

--- parse_parallel.py
+++ parse_parallel.py
@@ -1,4 +1,9 @@
+from pathlib import Path
 from stanfordnlp.server import CoreNLPClient
+from multiprocessing import Pool
+import itertools
+import datetime
+import os
 import gc
 
 class DocumentProcessor:
